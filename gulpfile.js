59960ec036baf5688f972dabd506528ed858e9d4
nat6 <antnatalia55@gmail.com>
2020-06-30 19:07:33 +0400

    init

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+var gulp = require('gulp'),
+		pug = require('gulp-pug'),
+		sass = require('gulp-sass');
+gulp.task('pug', function() {
+	return gulp.src('src/pug/pages/*.pug')
+		.pipe(pug({
+			pretty:true
+		}))
+		.pipe(gulp.dest('build/pages'));
+	});
+gulp.task('sass', function(){
+	return gulp.src('src/static/scss/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('build/styles'))
+});
+gulp.task('watch', function() {
+	gulp.watch('src/static/scss/*.scss', gulp.parallel('sass'));
+	gulp.watch('src/pug/pages/*.pug', gulp.parallel('pug'))
+});
+gulp.task( 'default', gulp.parallel('sass', 'pug', 'watch'));
